ec5b46fbcb5f6ed33db277453a10eeb6d74dfd54
Cas <617080+CasEbb@users.noreply.github.com>
2022-12-05 21:51:11 +0100

    Day 5

---
+++ 05/5b.py
@@ -0,0 +1,35 @@
+import re
+
+INSTRUCTION_REGEX = re.compile(r"move (\d+) from (\d) to (\d)")
+
+# Initial stacks from puzzle input
+stacks = [
+    list("RPCDBG"),
+    list("HVG"),
+    list("NSQDJPM"),
+    list("PSLGDCNM"),
+    list("JBNCPFLS"),
+    list("QBDZVGTS"),
+    list("BZMHFTQ"),
+    list("CMDBF"),
+    list("FCQG"),
+]
+
+# Read puzzle input
+lines = open("input").read().splitlines()
+
+# Skip first 10 lines
+lines = lines[10:]
+
+# Loop through instructions
+for line in lines:
+    num, src, dest = map(int, INSTRUCTION_REGEX.match(line).groups())
+    stacks[dest - 1].extend(stacks[src - 1][-num:])
+    del stacks[src - 1][-num:]
+
+# Print top of each stack
+for stack in stacks:
+    print(stack[len(stack) - 1], end="")
+
+# Print newline
+print()
